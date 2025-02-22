03e8ac75f2b01a0f482d8d2ecde0dce1ab56c21d
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-22 19:46:33 +0200

    add on boarding button

--- lib/feature/on_Boarding/presentation/screens/on_boarding_screen.dart
+++ lib/feature/on_Boarding/presentation/screens/on_boarding_screen.dart
@@ -1,11 +1,12 @@
 import 'package:charity_app/core/theme/app_colors.dart';
-import 'package:charity_app/core/theme/app_text_style.dart';
 import 'package:charity_app/feature/on_Boarding/logic/cubit/on_boarding_cubit.dart';
+import 'package:charity_app/feature/on_Boarding/presentation/widgets/on_boarding_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../../../core/routes/routes.dart';
+import '../widgets/description_onboarding.dart';
 import '../widgets/image_widget.dart';
 import '../widgets/text_onboarding_widgets.dart';
 
@@ -60,31 +61,10 @@ class OnBoardingScreen extends StatelessWidget {
                               TextOnboardingWidgets(onBoarding: onBoarding),
                               SizedBox(height: 20),
                               // AnimatedSwitcher for the description
-                              AnimatedSwitcher(
-                                duration: Duration(milliseconds: 500),
-                                transitionBuilder: (Widget child,
-                                    Animation<double> animation) {
-                                  return FadeTransition(
-                                    opacity: animation,
-                                    child: child,
-                                  );
-                                },
-                                child: Text(
-                                  key: ValueKey<int>(onBoarding
-                                      .currentIndex), // Unique key for animation
-                                  onBoarding
-                                      .onBoardingList[onBoarding.currentIndex]
-                                      .description,
-                                  textAlign: TextAlign.center,
-                                  style: AppTextStyle.poppins14W400blackColor,
-                                ),
-                              ),
+                              DescriptionOnboarding(onBoarding: onBoarding),
                               SizedBox(height: 20),
                               // Button to navigate to the next content
-                              SizedBox(
-                                width: double.infinity,
-                                height: 50,
-                                child: MaterialButton(
+                              OnBoardingButton(
                                   onPressed: () {
                                     if (onBoarding.currentIndex ==
                                         onBoarding.onBoardingList.length - 1) {
@@ -95,20 +75,10 @@ class OnBoardingScreen extends StatelessWidget {
                                       context.read<OnBoardingCubit>().next();
                                     }
                                   },
-                                  color: AppColors.primaryColor,
-                                  textColor: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(4),
-                                  ),
-                                  child: Text(
-                                    onBoarding.currentIndex ==
-                                            onBoarding.onBoardingList.length - 1
-                                        ? 'Start'
-                                        : 'Next',
-                                    style: AppTextStyle.poppins16W400whiteColor,
-                                  ),
-                                ),
-                              ),
+                                  text: onBoarding.currentIndex ==
+                                          onBoarding.onBoardingList.length - 1
+                                      ? 'Start'
+                                      : 'Next'),
                               SizedBox(height: 20),
                             ],
                           ),
