8879332c46997ff891ed0b86fa354191b6a33841
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-22 16:35:55 +0200

    add text of onBoarding screen in app text file

---
+++ lib/core/helpers/app_text.dart
@@ -0,0 +1,14 @@
+class AppText {
+  AppText._();
+
+  /// text of onBoarding screen
+  static const String onBoardingtextTitleOne = 'Browsing charities';
+  static const String onBoardingtextTitletwo = 'Payment Confirmation';
+  static const String onBoardingtextTitlethree = 'Donations History';
+  static const String onBoardingtextSubtitleOne =
+      'Users can see the name and logo of\n each charity, as well as a brief summary\n of its mission. This feature makes it easy\n for users to discover new charities that\n they may be interested in supporting';
+  static const String onBoardingtextSubtitleTwo =
+      'This feature helps users feel more\n confident that their donation has been\n properly received and processed. Users\n will receive a receipt for their donation\n that can be used for tax purposes.';
+  static const String onBoardingtextSubtitleThree =
+      'Users can see the date, amount and\n charity that was donated.\n This feature can be useful for users who\n want to track their charitable donations\n over time.';
+}
