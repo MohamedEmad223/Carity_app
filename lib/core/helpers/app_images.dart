ca9eb842cd6a96adf12a92323acb4db848fd3867
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-22 16:23:16 +0200

    add onBoarding images in assets

--- lib/core/helpers/app_images.dart
+++ lib/core/helpers/app_images.dart
@@ -1,6 +1,5 @@
 class AppImages {
   AppImages._();
-  // iamges path of splash screen 
-  static const String logo = 'assets/images/splash/athar 1.svg';
-  static const String logopng = 'assets/images/splash/athar 2.svg';
+  // iamges path of splash screen
+  static const String logopng = 'assets/images/splash/athar 1.png';
 }
