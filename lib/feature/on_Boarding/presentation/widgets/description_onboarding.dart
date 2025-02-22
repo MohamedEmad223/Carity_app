cf058fedfccc4c556efaf70bd03912d6d6625752
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-22 19:40:17 +0200

    add decription widgets

---
+++ lib/feature/on_Boarding/presentation/widgets/description_onboarding.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/theme/app_text_style.dart';
+import '../../logic/cubit/on_boarding_cubit.dart';
+
+class DescriptionOnboarding extends StatelessWidget {
+  const DescriptionOnboarding({super.key, required this.onBoarding});
+
+  final OnBoardingCubit onBoarding;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSwitcher(
+      duration: Duration(milliseconds: 500),
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        return FadeTransition(
+          opacity: animation,
+          child: child,
+        );
+      },
+      child: Text(
+        key: ValueKey<int>(onBoarding.currentIndex), 
+        onBoarding.onBoardingList[onBoarding.currentIndex].description,
+        textAlign: TextAlign.center,
+        style: AppTextStyle.poppins14W400blackColor,
+      ),
+    );
+  }
+}
