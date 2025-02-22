67860c680855479fbc093c2bc578b7b31c57416a
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-22 16:27:16 +0200

    add path of images into app images file

--- lib/core/helpers/app_images.dart
+++ lib/core/helpers/app_images.dart
@@ -1,5 +1,12 @@
 class AppImages {
   AppImages._();
-  // iamges path of splash screen
+  // imges path of splash screen
   static const String logopng = 'assets/images/splash/athar 1.png';
+  // imges path of onBoarding screen
+  static const String onBoarding2 =
+      'assets/images/onBoarding/onBoarding_two.png';
+  static const String onBoarding3 =
+      'assets/images/onBoarding/onBoarding_three.png';
+  static const String onBoarding1 =
+      'assets/images/onBoarding/onBoarding_one.png';
 }
