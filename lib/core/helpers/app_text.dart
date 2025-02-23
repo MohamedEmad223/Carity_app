1a222038362a7d4d554aa38ff352d408e72d170a
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-23 16:34:21 +0200

    add texts of login screen in app text file

--- lib/core/helpers/app_text.dart
+++ lib/core/helpers/app_text.dart
@@ -14,4 +14,16 @@ class AppText {
       'This feature helps users feel more\n confident that their donation has been\n properly received and processed. Users\n will receive a receipt for their donation\n that can be used for tax purposes.';
   static const String onBoardingtextSubtitleThree =
       'Users can see the date, amount and\n charity that was donated.\n This feature can be useful for users who\n want to track their charitable donations\n over time.';
+  static const String signInTextEmail = 'Sign In';
+  static const String enterYourInformation =
+      'Enter your information to open your account';
+  static const String signInTextSignIn = 'E-mail';
+  static const String signInTextPassword = 'Password';
+  static const String signInTextForgotPassword = 'Forgot your password ?';
+  static const String signUpTextFormFeildEmailSignUp = 'Enter your  e-mail';
+  static const String signUpTextFormFeildPasswordSignUp = 'Enter your password';
+  static const String donNotHaveAccount = 'Don\'t have an account ?';
+  static const String continueText = 'Continue';
+  static const String gust = 'a guest';
+  
 }
