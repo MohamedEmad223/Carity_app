5b4546e85847e16b331e89885fa77537c9bd7152
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-23 19:15:25 +0200

    finish text form feild widgets

--- lib/core/widgets/text_form_feild_widget.dart
+++ lib/core/widgets/text_form_feild_widget.dart
@@ -4,22 +4,38 @@ import '../theme/app_colors.dart';
 import '../theme/app_text_style.dart';
 
 class TextFormFeildWidget extends StatelessWidget {
-  const TextFormFeildWidget({super.key, required this.hintText, this.controller});
+  const TextFormFeildWidget(
+      {super.key,
+      required this.hintText,
+      this.controller,
+      required this.textInputType});
   final String hintText;
   final TextEditingController? controller;
-
-
+  final TextInputType textInputType;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      keyboardType: textInputType,
       controller: controller,
       decoration: InputDecoration(
         filled: true,
         fillColor: Colors.white,
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(
+            color: Colors.red,
+          ),
+        ),
         errorBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5),
           borderSide: BorderSide(
             color: Colors.red,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide(
+            color: AppColors.greyColor,
           ),
         ),
         focusedBorder: OutlineInputBorder(
@@ -29,8 +45,11 @@ class TextFormFeildWidget extends StatelessWidget {
           ),
         ),
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5),
-        ),
+            borderRadius: BorderRadius.circular(5),
+            borderSide: BorderSide(
+              color: AppColors.greyColor,
+            ),
+            gapPadding: 0),
         hintText: hintText,
         hintStyle: AppTextStyle.poppins14W400greyColor,
       ),
--- lib/feature/auth/login/presentation/widgets/login_body_screen.dart
+++ lib/feature/auth/login/presentation/widgets/login_body_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../core/helpers/app_text.dart';
-import '../../../../../core/theme/app_colors.dart';
 import '../../../../../core/theme/app_text_style.dart';
+import '../../../../../core/widgets/text_form_feild_widget.dart';
 import 'images_widgets.dart';
 import 'lable_text_form_feild.dart';
 
@@ -11,66 +11,50 @@ class LoginBodyScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ImagesWidgets(),
-        Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: Colors.white,
+    return Column(children: [
+      ImagesWidgets(),
+      Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                AppText.signInTextEmail,
+                style: AppTextStyle.poppins22W400blackColor,
+              ),
+              SizedBox(height: 10),
+              Text(
+                AppText.enterYourInformation,
+                style: AppTextStyle.poppins14W400blackColor,
+              ),
+              SizedBox(height: 25),
+              LableTextFormFeild(
+                lable: AppText.signInTextEmail,
+              ),
+              SizedBox(height: 5),
+              TextFormFeildWidget(
+                textInputType: TextInputType.emailAddress,
+                hintText: AppText.signUpTextFormFeildEmailSignUp,
+              ),
+              SizedBox(height: 10),
+              LableTextFormFeild(
+                lable: AppText.signInTextPassword,
+              ),
+              SizedBox(height: 5),
+              TextFormFeildWidget(
+                hintText: AppText.signUpTextFormFeildPasswordSignUp,
+                textInputType: TextInputType.visiblePassword,
+              ),
+            ],
           ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  AppText.signInTextEmail,
-                  style: AppTextStyle.poppins22W400blackColor,
-                ),
-                SizedBox(height: 10),
-                Text(
-                  AppText.enterYourInformation,
-                  style: AppTextStyle.poppins14W400blackColor,
-                ),
-                SizedBox(height: 25),
-                LableTextFormFeild(
-                  lable: AppText.signInTextEmail,
-                ),
-                SizedBox(height: 5),
-                TextFormField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.white,
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: BorderSide(
-                        color: Colors.red,
-                      ),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: BorderSide(
-                        color: AppColors.primaryColor,
-                      ),
-                    ),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    hintText: AppText.signUpTextFormFeildEmailSignUp,
-                    hintStyle: AppTextStyle.poppins14W400greyColor,
-                  ),
-                ),
-                SizedBox(height: 10),
-                LableTextFormFeild(
-                  lable: AppText.signInTextPassword,
-                ),
-              ],
-            ),
-          ),
-        )
-      ],
-    );
+        ),
+      ),
+    ]);
   }
 }
