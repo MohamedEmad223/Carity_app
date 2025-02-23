6d53cf1d92cb4dd0ddad34064bba1fdd1245a0ae
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-23 21:25:14 +0200

    add screen util to charity_app.dart  file

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'charity_app.dart';
 
-void main() {
+void main() async {
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
   runApp(const CharityApp());
 }
-
