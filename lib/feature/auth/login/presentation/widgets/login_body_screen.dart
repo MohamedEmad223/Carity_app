e5da8e316b20156ba312ea167d5a69389002b867
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-23 18:51:09 +0200

    add text form feild

--- lib/feature/auth/login/presentation/widgets/login_body_screen.dart
+++ lib/feature/auth/login/presentation/widgets/login_body_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../core/helpers/app_text.dart';
+import '../../../../../core/theme/app_colors.dart';
 import '../../../../../core/theme/app_text_style.dart';
 import 'images_widgets.dart';
 import 'lable_text_form_feild.dart';
@@ -11,41 +12,64 @@ class LoginBodyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: [
-          ImagesWidgets(),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
+      children: [
+        ImagesWidgets(),
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  AppText.signInTextEmail,
+                  style: AppTextStyle.poppins22W400blackColor,
+                ),
+                SizedBox(height: 10),
+                Text(
+                  AppText.enterYourInformation,
+                  style: AppTextStyle.poppins14W400blackColor,
+                ),
+                SizedBox(height: 25),
+                LableTextFormFeild(
+                  lable: AppText.signInTextEmail,
+                ),
+                SizedBox(height: 5),
+                TextFormField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    errorBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(
+                        color: Colors.red,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(
+                        color: AppColors.primaryColor,
+                      ),
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    hintText: AppText.signUpTextFormFeildEmailSignUp,
+                  ),
+                ),
+                SizedBox(height: 10),
+                LableTextFormFeild(
+                  lable: AppText.signInTextPassword,
+                ),
+              ],
             ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    AppText.signInTextEmail,
-                    style: AppTextStyle.poppins22W400blackColor,
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    AppText.enterYourInformation,
-                    style: AppTextStyle.poppins14W400blackColor,
-                  ),
-                  SizedBox(height: 25),
-                  LableTextFormFeild(
-                    lable: AppText.signInTextEmail,
-                  ),
-                  SizedBox(height: 10),
-                  LableTextFormFeild(
-                    lable: AppText.signInTextPassword,
-                  ),
-                ],
-              ),
-            ),
-          )
-        ],
-      );
+          ),
+        )
+      ],
+    );
   }
 }
