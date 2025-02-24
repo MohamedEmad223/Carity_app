c5a24c7913582aabd2fe6a7b8f8bf3233e6d2d32
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-24 18:06:20 +0200

    add forget password screen to routes

--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -4,6 +4,7 @@ class Routes {
   static const String onBoardingScreen = '/onBoarding';
   static const String loginScreen = '/login';
   static const String signUpScreen = '/signUp';
+  static const String forgetPasswordScreen = '/forgetPassword';
   static const String homeScreen = '/home';
 
 }
