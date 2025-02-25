956d6cbeee3e1c16fe73613d5500ec3ba561b9d4
DESKTOP-FH6ANST\Bakrow <mohamedelbakrawy70@gmail.com>
2025-02-25 19:27:22 +0200

    add verfication screen in files

---
+++ lib/feature/auth/forget_password/presentation/screens/verfication_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class VerficationScreen extends StatelessWidget {
+  const VerficationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Verification Screen'),
+      ),
+    );
+  }
+}
